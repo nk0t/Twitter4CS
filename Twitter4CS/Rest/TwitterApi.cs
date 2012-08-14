a47353cc8d6487bac592e8f35046286d872b153e
laco <sugashi.switch@gmail.com>
2012-08-14 12:05:58 +0900

    TwitterApiにUsersのAPIを追加

--- Twitter4CS/Rest/TwitterApi.cs
+++ Twitter4CS/Rest/TwitterApi.cs
@@ -97,6 +97,50 @@ namespace Twitter4CS.Rest
 			var url = TwitterApiUrl + string.Format("statuses/retweet/{0}.xml", statusId);
 			XDocument xdoc = oauth.RequestAPI(url, OAuth.RequestMethod.POST, param);
 			return Status.Create(xdoc.Root);
-		}
-	}
+        }
+
+        #region User
+
+        /// <summary>
+        /// ScreenNameからUserオブジェクトを生成
+        /// </summary>
+        /// <param name="oauth"></param>
+        /// <param name="screenName"></param>
+        /// <returns></returns>
+        public static User GetUser(this OAuth oauth, string screenName)
+        {
+            var param = new List<KeyValuePair<string, string>>();
+            param.Add(new KeyValuePair<string, string>("screen_name", screenName));
+            param.Add(new KeyValuePair<string, string>("include_entities", "true"));
+            var url = TwitterApiUrl + "users/show.xml";
+            XDocument xdoc = oauth.RequestAPI(url, OAuth.RequestMethod.GET, param);
+            return User.Create(xdoc.Root);
+        }
+
+        /// <summary>
+        /// ScreenName配列から最大100件までのUserオブジェクトを生成
+        /// </summary>
+        /// <param name="oauth"></param>
+        /// <param name="screenNames"></param>
+        /// <returns></returns>
+        public static ICollection<User> GetUsers(this OAuth oauth, string[] screenNames)
+        {
+            var param = new List<KeyValuePair<string, string>>();
+            var names = string.Join(",", screenNames);
+            param.Add(new KeyValuePair<string, string>("screen_name", names));
+            param.Add(new KeyValuePair<string, string>("include_entities", "true"));
+            var url = TwitterApiUrl + "users/lookup.xml";
+            XDocument xdoc = oauth.RequestAPI(url, OAuth.RequestMethod.GET, param);
+            IEnumerable<XElement> elements = from el in xdoc.Root.Elements("user") select el;
+            ICollection<User> users = new List<User>();
+            foreach (XElement el in elements)
+            {
+                users.Add(User.Create(el));
+            }
+            return users;
+        }
+
+
+        #endregion
+    }
 }
