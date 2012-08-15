67d96d02c68e1b9c522e79beb7ff5f3e7c6aeedf
nankotsu <nankotsu230@gmail.com>
2012-08-15 15:56:45 +0900

    feel

--- t4csConsole/Program.cs
+++ t4csConsole/Program.cs
@@ -7,6 +7,7 @@ using Twitter4CS;
 using Twitter4CS.Authentication;
 using System.Diagnostics;
 using Twitter4CS.Rest;
+using System.IO;
 
 namespace t4csConsole
 {
@@ -14,22 +15,40 @@ namespace t4csConsole
 	{
 		static void Main(string[] args)
 		{
-			OAuth account = new Account();
-			string token;
-			Uri url = account.GetAuthorizeUrl(out token);
-			Process.Start(url.ToString());
+			OAuth account;
 
-			string pin = Console.ReadLine();
-			long userId = 0;
-			string screenName = string.Empty;
-			if (account.GetAccessToken(token, pin, out userId, out screenName))
+			string token = "";
+			string secret = "";
+			if (File.Exists("token.txt"))
 			{
-				Console.WriteLine("認証成功");
+				string[] ts = File.ReadAllText("token.txt").Split(':');
+				token = ts[0];
+				secret = ts[1];
+				account = new Account(ts[0], ts[1]);
+				Console.WriteLine("認証完了");
 			}
 			else
 			{
-				Console.WriteLine("認証失敗");
-				return;
+				account = new Account();
+				Uri url = account.GetAuthorizeUrl(out token);
+				Process.Start(url.ToString());
+
+				string pin = Console.ReadLine();
+				long userId = 0;
+				string screenName = string.Empty;
+				if (account.GetAccessToken(token, pin, out userId, out screenName))
+				{
+					Console.WriteLine("認証成功");
+					if (!File.Exists("token.txt"))
+					{
+						File.WriteAllText("token.txt", string.Format("{0}:{1}", account.Token, account.Secret));
+					}
+				}
+				else
+				{
+					Console.WriteLine("認証失敗");
+					return;
+				}
 			}
 
 			while (true)
