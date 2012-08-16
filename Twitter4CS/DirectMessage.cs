5e116f54b83aba58885f76d2f49a663fe19fda22
laco <sugashi.switch@gmail.com>
2012-08-16 19:10:19 +0900

    DirectMessageとStatusのToStringを修正 TwitterApiにDM関連APIを追加 IdとIDの混在を修正(IDsクラス関連は除く) インデントスタイルの統一 Close #11

--- Twitter4CS/DirectMessage.cs
+++ Twitter4CS/DirectMessage.cs
@@ -7,37 +7,55 @@ using Twitter4CS.Util;
 
 namespace Twitter4CS
 {
-    public class DirectMessage
-    {
-        private DirectMessage()
-        {
-        }
+	public class DirectMessage
+	{
+		private DirectMessage()
+		{
+		}
 
-        public static DirectMessage Create(XElement node)
-        {
-            if (node == null)
-                throw new ArgumentNullException();
-            var message = new DirectMessage();
-            message.Id = node.Element("id").Value.ToLong();
-            message.Text = node.Element("text").ParseString();
-            message.Sender = User.Create(node.Element("sender"));
-            message.SenderId = node.Element("sender_id").Value.ToLong();
-            message.SenderScreenName = node.Element("sender_screen_name").Value;
-            message.Recipient = User.Create(node.Element("recipient"));
-            message.RecipientId = node.Element("recipient_id").Value.ToLong();
-            message.RecipientScreenName = node.Element("recipient_screen_name").Value;
-            message.CreatedAt = node.Element("created_at").Value.ToDateTime();
-            return message;
-        }
+		public static DirectMessage Create(XElement node)
+		{
+			if (node == null)
+				throw new ArgumentNullException();
+			var message = new DirectMessage();
+			message.Id = node.Element("id").Value.ToLong();
+			message.Text = node.Element("text").ParseString();
+			message.Sender = User.Create(node.Element("sender"));
+			message.SenderId = node.Element("sender_id").Value.ToLong();
+			message.SenderScreenName = node.Element("sender_screen_name").Value;
+			message.Recipient = User.Create(node.Element("recipient"));
+			message.RecipientId = node.Element("recipient_id").Value.ToLong();
+			message.RecipientScreenName = node.Element("recipient_screen_name").Value;
+			message.CreatedAt = node.Element("created_at").Value.ToDateTime();
+			return message;
+		}
 
-        public long Id { get; private set; }
-        public string Text { get; private set; }
-        public User Sender { get; private set; }
-        public long SenderId { get; private set; }
-        public string SenderScreenName { get; private set; }
-        public User Recipient { get; private set;}
-        public long RecipientId { get; private set; }
-        public string RecipientScreenName { get; private set; }
-        public DateTime CreatedAt { get; private set; }
-    }
+		public long Id { get; private set; }
+		public string Text { get; private set; }
+		public User Sender { get; private set; }
+		public long SenderId { get; private set; }
+		public string SenderScreenName { get; private set; }
+		public User Recipient { get; private set;}
+		public long RecipientId { get; private set; }
+		public string RecipientScreenName { get; private set; }
+		public DateTime CreatedAt { get; private set; }
+
+		public override bool Equals(object obj)
+		{
+			if (obj is DirectMessage)
+				return Id == ((DirectMessage)obj).Id;
+			else
+				return false;
+		}
+
+		public override string ToString()
+		{
+			return SenderScreenName + ":" + Text;
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+	}
 }
--- Twitter4CS/Status.cs
+++ Twitter4CS/Status.cs
@@ -18,34 +18,34 @@ namespace Twitter4CS
 			if (node == null)
 				throw new ArgumentNullException();
 			var status = new Status();
-            status.Id = node.Element("id").Value.ToLong();
-            status.Text = node.Element("text").ParseString();
-            status.User = User.Create(node.Element("user"));
-            status.CreatedAt = node.Element("created_at").Value.ToDateTime();
-            status.Source = node.Element("source").ParseString();
-            status.InReplyToStatusId = node.Element("in_reply_to_status_id").Value.ToLong();
-            status.InReplyToUserId = node.Element("in_reply_to_user_id").Value.ToLong();
-            status.InReplyToUserScreenName = node.Element("in_reply_to_screen_name").ParseString();
+			status.Id = node.Element("id").Value.ToLong();
+			status.Text = node.Element("text").ParseString();
+			status.User = User.Create(node.Element("user"));
+			status.CreatedAt = node.Element("created_at").Value.ToDateTime();
+			status.Source = node.Element("source").ParseString();
+			status.InReplyToStatusId = node.Element("in_reply_to_status_id").Value.ToLong();
+			status.InReplyToUserId = node.Element("in_reply_to_user_id").Value.ToLong();
+			status.InReplyToUserScreenName = node.Element("in_reply_to_screen_name").ParseString();
 			status.RetweetedOriginal = node.Element("retweeted_status") == null ? null : Status.Create(node.Element("retweeted_status"));
 			status.RetweetedCount = node.Element("retweeted_count") == null ? 0 : node.Element("retweeted_count").Value.ToLong();
-            var urls = node.Element("entities").Element("urls");
-            IEnumerable<XElement> urlElements = from el in urls.Elements("url") select el;
-            foreach (XElement el in urlElements)
-            {
-                status.UrlEntities.Add(UrlEntity.Create(el));
-            }
-            var mentions = node.Element("entities").Element("user_mentions");
-            IEnumerable<XElement> mentionElements = from el in mentions.Elements("user_mention") select el;
-            foreach (XElement el in mentionElements)
-            {
-                status.UserMentionEntities.Add(UserMentionEntity.Create(el));
-            }
-            var hashtags = node.Element("entities").Element("hashtags");
-            IEnumerable<XElement> hashtagElements = from el in hashtags.Elements("hashtag") select el;
-            foreach (XElement el in hashtagElements)
-            {
-                status.hashtagEntities.Add(HashtagEntity.Create(el));
-            }
+			var urls = node.Element("entities").Element("urls");
+			IEnumerable<XElement> urlElements = from el in urls.Elements("url") select el;
+			foreach (XElement el in urlElements)
+			{
+				status.UrlEntities.Add(UrlEntity.Create(el));
+			}
+			var mentions = node.Element("entities").Element("user_mentions");
+			IEnumerable<XElement> mentionElements = from el in mentions.Elements("user_mention") select el;
+			foreach (XElement el in mentionElements)
+			{
+				status.UserMentionEntities.Add(UserMentionEntity.Create(el));
+			}
+			var hashtags = node.Element("entities").Element("hashtags");
+			IEnumerable<XElement> hashtagElements = from el in hashtags.Elements("hashtag") select el;
+			foreach (XElement el in hashtagElements)
+			{
+				status.hashtagEntities.Add(HashtagEntity.Create(el));
+			}
 			return status;
 		}
 
@@ -61,31 +61,30 @@ namespace Twitter4CS
 		public long InReplyToUserId { get; private set; }
 		public string InReplyToUserScreenName { get; private set; }
 		public Status RetweetedOriginal { get; private set; }
-        public long RetweetedCount { get; private set; }
+		public long RetweetedCount { get; private set; }
 
-        public override bool Equals(object obj)
-        {
-            if (obj is Status)
-                return Id == ((Status)obj).Id;
-            else 
-                return false;
-        }
+		public override bool Equals(object obj)
+		{
+			if (obj is Status)
+				return Id == ((Status)obj).Id;
+			else 
+				return false;
+		}
 
-        public override string ToString()
-        {
-            var status = this;
-            if (RetweetedOriginal.Id > 0)
-            {
-                status = RetweetedOriginal;
-            }
-            return status.User.ScreenName + ":" + status.Text 
-                + " [http://twitter.com/" +status.User.ScreenName + "/status/" + status.Id + "]";
-        }
+		public override string ToString()
+		{
+			var status = this;
+			if (RetweetedOriginal.Id > 0)
+			{
+				status = RetweetedOriginal;
+			}
+			return status.User.ScreenName + ":" + status.Text;
+		}
 
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-        
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+		
 	}
 }
